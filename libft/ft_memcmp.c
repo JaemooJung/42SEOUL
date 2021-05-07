a22cbf66c190b30f0b4bcc2e4800bcc74e93a280
Jaemoo Jung <jaemjung@c1r6s1.42seoul.kr>
2021-05-07 12:45:25 +0900

    add ft_memcmp

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaemjung <jaemjung@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/07 12:07:52 by jaemjung          #+#    #+#             */
+/*   Updated: 2021/05/07 12:07:52 by jaemjung         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*casted_s1;
+	unsigned char	*casted_s2;
+	size_t			i;
+
+	casted_s1 = s1;
+	casted_s2 = s2;
+	while (i < n)
+	{
+		if (casted_s1[i] != casted_s2[i])
+		{
+			return (casted_s1[i] - casted_s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
