6d5257371ddc1db587bf76786b3aaf8c398dea0d
Jaemoo Jung <jaemjung@c8r7s10.42seoul.kr>
2021-05-18 17:03:30 +0900

    je suis un con

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jaemjung <jaemjung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 12:27:36 by jaemjung          #+#    #+#             */
-/*   Updated: 2021/05/18 12:53:50 by jaemjung         ###   ########.fr       */
+/*   Updated: 2021/05/18 17:02:50 by jaemjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 int	get_next_line(int fd, char **line)
 {
-	
+	static char	*file_content;
+	char		*buff;
+	ssize_t		read_size;
+
+	if (fd < 0 || !line || BUFFER_SIZE < 1 ||
+	!(buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char))))
+		return (LINE_ERROR);
+	while (read_size = read(fd, buff, BUFFER_SIZE) > 0)
+	{
+		if (!file_content)
+			file_content = ft_strdup(buff);
+		else
+			ft_strlcpy(file_content, buff, read_size + 1);
+		if (ft_strchr())
+	}
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,15 +6,26 @@
 /*   By: jaemjung <jaemjung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 12:36:09 by jaemjung          #+#    #+#             */
-/*   Updated: 2021/05/18 12:37:45 by jaemjung         ###   ########.fr       */
+/*   Updated: 2021/05/18 15:39:50 by jaemjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define BUFFER_SIZE 32
+# define LINE_READ 1
+# define LINE_EOF 0
+# define LINE_ERROR -1
+
 # include <unistd.h>
+# include <stdlib.h>
 
-
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(const char *s1);
 
 #endif
