2a008a95d4fa361027866e04fbce8089b5102f81
Jaemoo Jung <jaemjung@c7r3s1.42seoul.kr>
2021-07-05 18:36:08 +0900

    libc printf test

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -3,8 +3,11 @@
 int main(void)
 {
 	ft_printf("Good Morning\n");
-	printf("int : |%8d|\n", 12345);
-	printf("char : |%8c|\n", 67);
-	printf("str : |%8s|\n", "hello world");
-	printf("float : |%0100f|\n", 123.456);
+
+	printf("int : |%.d|\n", 12345);
+	printf("unsigntd int : |%.3u|\n", 1234567891);
+	printf("char : |%c|\n", 'b');
+	printf("str : |%20s|\n", "hello world");
+	printf("ptr : |%p|\n", "HELLO");
+	printf("16 : |%-30x|\n", 16);
 }
