b1de734260939c9d43bfef3070ad004eb61f0914
Jaemoo Jung <jaemjung@c1r1s7.42seoul.kr>
2021-11-16 17:26:56 +0900

    fix : norm error fixed

--- circle_2/push_swap/sources/main.c
+++ circle_2/push_swap/sources/main.c
@@ -6,13 +6,13 @@
 /*   By: jaemjung <jaemjung@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 17:16:18 by jaemjung          #+#    #+#             */
-/*   Updated: 2021/11/16 17:16:29 by jaemjung         ###   ########.fr       */
+/*   Updated: 2021/11/16 17:22:05 by jaemjung         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc < 2)
 		error_handler();
