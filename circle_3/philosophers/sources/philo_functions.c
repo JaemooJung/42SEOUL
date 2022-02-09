77a76ca8c236e268d3568b50a7a8dda6076503d8
JaemooJung <dbdbwer@naver.com>
2022-02-09 15:47:14 +0900

    added usleep(10) on philo_think

--- circle_3/philosophers/sources/philo_functions.c
+++ circle_3/philosophers/sources/philo_functions.c
@@ -6,7 +6,7 @@
 /*   By: jaemoojung <jaemoojung@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 12:53:27 by jaemjung          #+#    #+#             */
-/*   Updated: 2022/02/09 15:22:20 by jaemoojung       ###   ########.fr       */
+/*   Updated: 2022/02/09 15:46:51 by jaemoojung       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int philo_sleep(t_philosopher *philo)
 int philo_think(t_philosopher *philo)
 {
 	philo_print(philo, THINK);
+	usleep(10);
 	return (0);
 }
 
