866fe336b4c4a4aba8b13fd1e358a8c8aec049f8
JaemooJung <dbdbwer@naver.com>
2022-04-13 22:28:01 +0900

    cpp start

---
+++ circle_4/c++/module00/ex00/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		std::cout << "*LOUD AND UNBEARABLE FEEDBACK NOISE*" << std::endl;
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			std::cout << static_cast<char>(std::toupper(argv[i][j]));
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
