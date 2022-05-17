c1818034d4df4bf15a3141f3b985aa34c70e0187
Jaemoo Jung <jaemjung@c3r3s2.42seoul.kr>
2022-05-17 17:32:27 +0900

    module04 ex00 done

---
+++ circle_4/c++/module04/ex00/Animal.hpp
@@ -0,0 +1,23 @@
+#ifndef __ANIMAL_H__
+#define __ANIMAL_H__
+
+#include <string>
+#include <iostream>
+
+class Animal {
+	protected:
+		std::string _type;
+	
+	public:
+		std::string getType() const;
+		void setType(std::string type);
+		virtual void makeSound() const;
+
+		Animal();
+		Animal(std::string type);
+		Animal(const Animal& other);
+		Animal& operator=(const Animal& other);
+		virtual ~Animal();
+};
+
+#endif // __ANIMAL_H__
---
+++ circle_4/c++/module04/ex00/Cat.hpp
@@ -0,0 +1,16 @@
+#ifndef __CAT_H__
+#define __CAT_H__
+
+#include "Animal.hpp"
+
+class Cat : public Animal {
+	public:
+		void makeSound() const;
+	
+		Cat& operator=(const Cat& other);
+		Cat();
+		Cat(const Cat& other);
+		virtual ~Cat();
+};
+
+#endif // __CAT_H__
